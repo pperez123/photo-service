6b8dc42bf917e80cdda1c42bee888bcc05327236
pperez <pperez@slipsleeve.com>
2014-08-09 22:59:03 -0700

    Add REST file operations

--- photoService/src/com/pperez/photoService/rest/FileService.java
+++ photoService/src/com/pperez/photoService/rest/FileService.java
@@ -8,21 +8,27 @@ import java.io.File;
 import javax.servlet.ServletContext;
 import javax.websocket.server.PathParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.glassfish.jersey.media.multipart.file.DefaultMediaTypePredictor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.pperez.photoService.rest.util.FileStreamingOutput;
 import com.pperez.photoService.servlet.UploadService;
 
 /**
- * @author Philip 
- * Perez Aug 9, 2014 
- * FileService.java
+ * @author Philip
+ *         Perez Aug 9, 2014
+ *         FileService.java
  */
 @Path("/file/{filename}")
 public class FileService {
@@ -30,30 +36,43 @@ public class FileService {
 
     // location to store file uploaded
     private static final String UPLOAD_DIRECTORY = "upload";
+    
     @Context
     ServletContext servletContext;
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getFile(@PathParam("filename") String fileName) {
+    public Response getFile(@PathParam("filename") String fileName) throws WebApplicationException {
         String uploadDirectory = uploadDirectory();
-
+        FileStreamingOutput fileOutput = null;
+        
         if (uploadDirectory != null) {
             String filePath = uploadDirectory + File.separator + fileName;
             File file = new File(filePath);
-
+            
             if (file.exists()) {
-
+                fileOutput = new FileStreamingOutput(file);
+            } else {
+                throw new WebApplicationException(Status.NOT_FOUND);
             }
         }
 
-        return null;
+        return responseWithEntity(fileOutput, DefaultMediaTypePredictor.CommonMediaTypes.
+                getMediaTypeFromFileName(fileName)).build();
     }
 
     @OPTIONS
-    public String returnOptions() {
-
-        return null;
+    @HEAD
+    public Response returnOptions() {
+        Response response = Response.ok()
+            .header("Pragma", "no-cache")
+            .header("Cache-Control", "no-store, no-cache, must-revalidate")
+            .header("X-Content-Type-Options", "nosniff") // Prevent IE from MIME sniffing the content
+            .header("Access-Control-Allow-Origin", "*") // Allow cross domain resource sharing
+            .header("Access-Control-Allow-Credentials", "true")
+            .header("Access-Control-Allow-Methods", "OPTIONS,HEAD,GET,POST,PUT,PATCH,DELETE")
+            .header("Access-Control-Allow-Headers", "Content-Type,Content-Disposition,Content-Range")
+            .build();
+        return response;
     }
 
     // constructs the directory path to store upload file
@@ -69,5 +88,12 @@ public class FileService {
         }
 
         return uploadPath;
+    }
+    
+    protected Response.ResponseBuilder responseWithEntity(Object entity, MediaType type) {
+        return Response.ok(entity, type)
+                .header("Pragma", "no-cache")
+                .header("Cache-Control", "no-store, no-cache, must-revalidate")
+                .header("X-Content-Type-Options", "nosniff"); // Prevent IE from MIME sniffing the content
     }
 }
---
+++ photoService/src/com/pperez/photoService/rest/util/FileStreamingOutput.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.pperez.photoService.rest.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.StreamingOutput;
+
+/**
+ * @author pperez
+ *
+ */
+public class FileStreamingOutput implements StreamingOutput {
+    private File file;
+
+    public FileStreamingOutput(File file) {
+        this.file = file;
+    }
+
+    @Override
+    public void write(OutputStream output) throws IOException,
+            WebApplicationException {
+        FileInputStream input = new FileInputStream(file);
+        try {
+            int bytes;
+            while ((bytes = input.read()) != -1) {
+                output.write(bytes);
+            }
+        }
+        catch (Exception e) {
+            throw new WebApplicationException(e);
+        }
+        finally {
+            if (output != null)
+                output.close();
+            if (input != null)
+                input.close();
+        }
+    }
+}
